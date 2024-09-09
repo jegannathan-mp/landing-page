89f4d1e53642ca316bedd974c9f9ca62157913ae
jegannathan-mp <mpjegannathanmech@gmail.com>
2024-09-09 14:10:29 +0530

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import LoginPopup from './components/LoginPopup/LoginPopup';
